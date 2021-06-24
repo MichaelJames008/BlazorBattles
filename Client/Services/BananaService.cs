12aae0197cc373f5ea1f39b16c6757724f83c58d
Ela, Michael C <Michael.Ela@sykes.com>
2021-06-24 10:54:30 -0400

    Added Eat and Add Bananas Methods

---
+++ Client/Services/BananaService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public class BananaService : IBananaService
+    {
+        public event Action OnChange;
+
+        void BananaChanged() => OnChange.Invoke();
+        public int Bananas { get; set; }
+
+        public void EatBananas(int amount)
+        {
+            Bananas -= amount;
+            BananaChanged();
+        }
+
+        public void AddBananas(int amount)
+        {
+            Bananas += amount;
+            BananaChanged();
+        }
+    }
+}
